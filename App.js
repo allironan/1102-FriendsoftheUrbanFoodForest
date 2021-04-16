1fb083bff015bfe0af569cce42be2c3ace0e9949
Praneet Kedari <praneet.kedari@gmail.com>
2021-04-16 11:42:25 -0400

    moved authentication keys to .env file so it doesn't get on repo

--- App.js
+++ App.js
@@ -4,18 +4,24 @@ import LoadingScreen from './screens/LoadingScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import HomeScreen from './screens/HomeScreen'
-
+import { FIREBASE_APIKEY,
+        FIREBASE_AUTHDOMAIN,
+        FIREBASE_PROJECTID,
+        FIREBASE_STORAGEBUCKET,
+        FIREBASE_MESSAGINGSENDERID,
+        FIREBASE_APPID} from '@env'
 import * as firebase from 'firebase'
 
-var firebaseConfig = {
-  apiKey: "AIzaSyDA2SXeoLpH1bUlYdDpJzMo-6WPN2sPMKM",
-  authDomain: "friends-of-the-urban-ff.firebaseapp.com",
-  projectId: "friends-of-the-urban-ff",
-  storageBucket: "friends-of-the-urban-ff.appspot.com",
-  messagingSenderId: "1014426608257",
-  appId: "1:1014426608257:web:8ff56858313883ad05a380"
-};
 
+firebaseConfig = 
+{
+  apiKey: FIREBASE_APIKEY,
+  authDomain: FIREBASE_AUTHDOMAIN,
+  projectId:  FIREBASE_PROJECTID,
+  storageBucket: FIREBASE_STORAGEBUCKET,
+  messagingSenderId: FIREBASE_MESSAGINGSENDERID,
+  appId: FIREBASE_APPID
+}
 firebase.initializeApp(firebaseConfig);
 
 const AppStack = createStackNavigator({
