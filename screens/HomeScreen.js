fee8e6afcd1f9c36784731ffa6cab86d3696f167
mfarias6 <matthewgfarias@gmail.com>
2021-09-12 16:33:05 -0400

    Current Front-End Update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Children } from 'react'
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 import firebase from 'firebase/app'
 import 'firebase/auth'
@@ -17,17 +17,26 @@ export default class HomeScreen extends React.Component {
     }
 
     currentView() {
-        return <View style={styles.container}> 
-            <Text> Friends of the Urban Food Forest </Text>
-            <div>
-                <TouchableOpacity onPress={this.createPostsPressed}>
-                    <Text> Add Posts </Text>
+        return (
+        <View style={styles.container}>
+                <View style={{ padding: 10, flex: 1}}>
+                    <Text style= {styles.title}> Friends of the Urban Food Forest </Text>
+                </View>
+                <TouchableOpacity 
+                    onPress={this.createPostsPressed}
+                    style={styles.addPostButton}
+                >
+                    <Text style={styles.addPostLabel}> Add Post </Text>
                 </TouchableOpacity>
-            </div>
-            <div id="postsArray">
-            </div>
-
-        </View> 
+                <View style={styles.postFrame}>
+                    <Text style={styles.postTitle}>Test Title</Text>
+                    <Text style={styles.postDate}>August 12th, 4:25pm</Text>
+                    <Text style={styles.postContent}>Test Content</Text>
+                </View>
+                <div id="postsArray">
+                </div>
+        </View>
+        );
     } 
 
     componentDidMount() {
@@ -40,22 +49,85 @@ export default class HomeScreen extends React.Component {
     signOutUser = () => {
         firebase.default.auth().signOut()
     }
+
     render() {
         return this.currentView();
     }
 
     createPostsPressed = () => {
-       makeNewPost(this.state.title, this.state.contents);
-       //deletePost(this.state.postID);
-    }
+        makeNewPost(this.state.title, this.state.contents);
+        //deletePost(this.state.postID);
+     }
 }
+
+/*class Post {
+    constructor(title, contents, postID)
+} */
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center" 
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'rgba(233,243,196,1)'
+    },
+    title: {
+        textAlign: "center",
+        padding: 20,
+        marginBottom: 60,
+        fontSize: 36,
+        fontWeight: 'bold',
+        flexWrap: "wrap"
+    },
+    addPostButton: {
+        backgroundColor: 'rgba(255,255,255,1)',
+        borderRadius: 8,
+        alignSelf: 'flex-end',
+        width: 100,
+        paddingVertical: 10,
+        marginHorizontal: '2%',
+        marginBottom: 20,
+        textAlign: 'center',
+        textAlignVertical: 'center'
+    },
+    addPostLabel: {
+        fontSize: 16,
+        color: 'rgba(196,196,196,1)'
+    },
+    postFrame: {
+        backgroundColor: 'rgba(255,255,255,1)',
+        borderRadius: 25,
+        width: 325,
+        height: 'auto',
+        minHeight: 150,
+        marginHorizontal: '2%',
+        marginBottom: 12,
+        textAlign: 'center'
+    },
+    postTitle: {
+        fontSize: 24,
+        textAlign: 'left',
+        flexWrap: 'wrap',
+        marginHorizontal: '8%',
+        marginTop: 15,
+        marginVertical: '2%',
+        fontWeight: 'Bold'
+    },
+    postDate: {
+        fontSize: 10,
+        textAlign: 'left',
+        flexWrap: 'wrap',
+        marginHorizontal: '8%',
+        marginBottom: 20,
+        color: 'rgba(196,196,196,1)'
+    },
+    postContent: {
+        fontSize: 12,
+        textAlign: 'left',
+        flexWrap: 'wrap',
+        marginHorizontal: '8%',
+        marginVertical: '5%',
+        marginBottom: 30
     }
-    
-    
 })
