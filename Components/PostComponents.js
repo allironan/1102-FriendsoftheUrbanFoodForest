33a2ce6d5e557b7e2e0562d46d9e831e2c5faa04
Andrew Novokshanov <77639938+A-Novokshanov@users.noreply.github.com>
2021-11-27 19:21:08 -0500

    Adds PostComponent Comments and DonationScreen Comments

--- Components/PostComponents.js
+++ Components/PostComponents.js
@@ -2,7 +2,23 @@ import firebase from 'firebase/app'
 import 'firebase/database';
 import 'firebase/firestore';
 
-//Function to make new posts
+/*
+The PostComponents class consists of many functions for manipulating instances 
+of a Post object using Firebase.
+*/ 
+
+/*
+ Function that creates a new post and adds the post to Firebase
+
+    Args:
+        title (string): The desired title of the new post
+        contents (string): The desired contents of the new post
+        survey (string): The url to a survey to be included in the post.
+            If none is provided, defaults to no survey.
+
+    Returns:
+        map: A map of properties that describe the newly created post
+*/
 export async function makeNewPost(title, contents, survey=null) {
 
     const db = firebase.firestore();
@@ -29,7 +45,13 @@ export async function makeNewPost(title, contents, survey=null) {
     return data;
 }
 
-// Function to get next post in database
+/*
+ Function that increments the id to be used in the next post
+
+    Returns:
+        integer: An integer value that represents a unique id
+        to identify the next post to be made
+*/
 async function getNextPost() {
 
     const db = firebase.firestore();
@@ -52,14 +74,18 @@ async function getNextPost() {
     } else {
         const res = db.collection('Counters').doc('Post Count');
         const increment = firebase.firestore.FieldValue.increment(1);
-        //const count = res.update("NextPostID", admin.firestore.FieldValue.increment(1));
         await res.update({NextPostID: increment})
         const count = await countRef.get();
         return count.data();
     }
 }
 
-//Function to get all posts in database
+/*
+ Function that retrieves all Posts that are located in Firebase
+
+    Returns:
+        array: an Array of all post details that were found in Firebase
+*/
 export async function getPosts() {
     const db = firebase.firestore();
 
@@ -69,7 +95,6 @@ export async function getPosts() {
         console.log("No posts in firebase");
         return null;
     } else {
-        // Attempt 4
         const postArray = [];
         console.log("The array of posts is below: ");
         console.log(postArray);
@@ -78,6 +103,15 @@ export async function getPosts() {
     }
 }
 
+/*
+ Function that edits a post in Firebase
+
+    Args:
+        title (string): The desired title of the post
+        contents (string): The desired contents of the post
+        survey (string): The url to a survey to be included in the post
+        postID (string): The id of the post that is to be edited
+*/
 export async function editPost(title, contents, survey, postID) {
 
     const currentUser = firebase.auth().currentUser;
@@ -99,14 +133,10 @@ export async function editPost(title, contents, survey, postID) {
 
 //Function to delete posts
 /*
- Function that deletes posts from Firebase with provided ID
+ Function that deletes a post from Firebase
 
     Args:
-        postID (string): A list of (x_test, y_test, predictions) tuples
-
-    Returns:
-        list: A list of silhouette scores, returns a list because there is a
-              a recall rate for each for the folds in K-fold
+        postID (string): The id of the post that is to be deleted
 */
 export async function deletePost(postID) {
     const db = firebase.firestore();
--- screens/FundraisingScreens/DonationScreen.js
+++ screens/FundraisingScreens/DonationScreen.js
@@ -1,47 +1,58 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Text, TouchableOpacity, View, TextInput, BackHandler } from 'react-native';
 import styles from '../styles/FundraisingScreens.styles';
 import { PayPalButton } from 'react-paypal-button-v2';
-import { CurrencyInput } from "react-currency-input-field";
+
+/*
+The DonationScreen class renders the donation screen to the user.
+Additionally, this screen is responsible for donation transactions through Paypal
+*/ 
 export default class DonationScreen extends React.Component {
     state = {
-        donation: "0.00"
+        donation_amount: "0.00"
     }
 
     currentView() {
         return (
             <View style={styles.container}>
+                {/* Creates a Button that allows a user to return back to the Fundraising page*/}
                 <TouchableOpacity style={styles.functionButton} onPress={() => this.props.navigation.goBack()}>
                     <Text> Go back </Text>
                 </TouchableOpacity>
-                {/* Creates a Text Input Field that sets the value of donation to a positive number*/}
+                {/* Creates a Text Input Field that allows the user to set the value of donation.
+                This value entered by the user will be positive and numerical*/}
                 <TextInput 
                     style={styles.textFillField}
                     placeholder="Donation Amount" 
-                    value={"$" + this.state.donation}
+                    value={"$" + this.state.donation_amount}
                     keyboardType = 'numeric'
                     onChangeText={
-                        (value) => this.setState({donation: parseFloat(value.replace(/[^0-9.]/g, '')).toFixed(2)})
+                        (value) => this.setState({donation_amount: parseFloat(value.replace(/[^0-9.]/g, '')).toFixed(2)})
                     } 
                 />
+                {/* Utilizing react-paypal-button-v2 we render a number of options for the user
+                to complete a transaction based on the value currently in the donation input.
+                Users can choose from paying with a Paypal account, paying later, or with a debit 
+                or credit card*/}
                 <PayPalButton
                     createOrder={(data, actions) => {
                         return actions.order.create({
                             purchase_units: [{
                                 amount: {
                                     currency_code: "USD",
-                                    value: this.state.donation
+                                    value: this.state.donation_amount
                                 },
                                 description: "Donation to Friends of the Urban Food Forest at Brown Mills",
                             }]
                         });
                     }}
                     onSuccess={(details, data) => {
-                        alert("Thank you for your generous donation " + details.payer.name.given_name + "!");
+                        alert("Thank you for your generous donation of $" + this.state.donation_amount + " " + details.payer.name.given_name + "!");
                         this.props.navigation.goBack()
                     }}
                     currency = "USD"
                     options={{
+                        // Not the real client ID for security reasons
                         clientId: "AYz9JKsZ_TEo5elY-FFAuJRZBg9Nz5cbIhDQp-kGWX_W9SOA5_mAy0TAQ8ZQ1puEWkYJ9czpV9m_2deL",
                         currency: "USD"
                     }}
